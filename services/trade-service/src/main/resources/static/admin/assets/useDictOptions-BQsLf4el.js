ba6d1c1e6ff6f8cb6c0838db24f436e032bfcb9c
cwx <changwx01@jundeyaoxin.com>
2026-09-23 14:11:47 +0800

    chore(admin): 重建 static/admin 产物

---
+++ services/trade-service/src/main/resources/static/admin/assets/useDictOptions-BQsLf4el.js
@@ -0,0 +1,1 @@
+import{k as o,r}from"./index-CByO5bOR.js";import{c as i}from"./ui-vendor-DdIZWGRH.js";function s(t){return i(()=>(o(),r(t)))}export{s as u};
---
+++ services/trade-service/src/main/resources/static/admin/assets/useErrorPageActions-Cx1WXAId.js
@@ -0,0 +1,1 @@
+import{j as c}from"./ui-vendor-DdIZWGRH.js";import{c as s,H as a}from"./index-CByO5bOR.js";function k(){const o=c(),n=s();function t(){return a(n)}function r(){o.replace(t())}function u(){var e;if(((e=window.history.state)==null?void 0:e.back)!=null){o.back();return}r()}return{resolveHome:t,goHome:r,goBack:u}}export{k as u};
